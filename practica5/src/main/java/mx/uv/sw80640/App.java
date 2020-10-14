6fa883c1496d88a1efb3b3dbd2f562e4249de1e5
ManuelHRdg <manuel.gat1@gmail.com>
2020-10-14 12:49:43 -0500

    Se agregaron metodos nuevos

--- practica5/src/main/java/mx/uv/sw80640/App.java
+++ practica5/src/main/java/mx/uv/sw80640/App.java
@@ -11,7 +11,24 @@ public class App
     public static void main( String[] args )
     {
         //System.out.println( "Hello World!" );
-        get("/", (req, res) -> "Hola desde Spark");
+        //get("/", (req, res) -> "Hola desde Spark");
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+        
         get("/hola", (req, res) -> "Hola hola");
         get("/adios", (req, res) -> "Adios desde Spark");
     }
